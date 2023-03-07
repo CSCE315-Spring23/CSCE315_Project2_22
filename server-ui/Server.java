399e9e6f34dd0c827e5b9b3d9bd43a75f10a621d
brianminhvu1 <123588431+brianminhvu1@users.noreply.github.com>
2023-03-07 15:16:49 -0600

    Complete Order List and Price

--- server-ui/Server.java
+++ server-ui/Server.java
@@ -6,14 +6,26 @@ import java.util.ArrayList;
 
 public class Server {
     // class to store an item in an order
-    private class OrderItem {
-        // TODO
+    private static class OrderItem {
+        // TODOs
+        String order_name;
+        String order_price;
+
+        public OrderItem (String name, String price) {
+            order_name = name;
+            order_price = price;
+        }
     }
+
+    
     public static void main(String[] args) {
         // INFORMATION
         ArrayList<String> categories = new ArrayList<String>();
         ArrayList<ArrayList<String>> menu_items = new ArrayList<ArrayList<String>>();
-        //ArrayList<String> order_items;
+        ArrayList<OrderItem> order_items = new ArrayList<OrderItem>();
+        ArrayList<Double> order_prices = new ArrayList<Double>();
+
+        
 
 
         // ESTABLISH DATABASE CONNECTION
@@ -41,13 +53,14 @@ public class Server {
 			}
 
             // menu information
-			sqlStatement = "SELECT category, menu_item_id FROM menu;";
+			sqlStatement = "SELECT category, menu_item_id, price FROM menu;";
 			result = stmt.executeQuery(sqlStatement);
 
 			while (result.next()) {
                 ArrayList<String> row_item = new ArrayList<String>();
                 row_item.add(result.getString("category"));
                 row_item.add(result.getString("menu_item_id"));
+                row_item.add(result.getString("price"));
 
                 menu_items.add(row_item);
 			}
@@ -67,10 +80,13 @@ public class Server {
 
 
         // INITIALIZE MAIN PANELS
-        JPanel left_panel = new JPanel();      
-        left_panel.setBackground(Color.gray);  
+        JPanel left_panel = new JPanel();
+        left_panel.setLayout(new BoxLayout(left_panel, BoxLayout.Y_AXIS)); // Set the layout manager to a vertical BoxLayout
+        left_panel.setBackground(Color.gray);      
+        left_panel.setPreferredSize(new Dimension(500, 190));
+        
 
-        JPanel right_panel = new JPanel(new BorderLayout());      
+        JPanel right_panel = new JPanel(new GridLayout());      
         right_panel.setBackground(Color.gray);
 
 
@@ -97,17 +113,66 @@ public class Server {
 
 
         // INITIALIZE SUB PANELS
-        JTabbedPane menu_pane = new JTabbedPane();  
+        
+        JTabbedPane menu_pane = new JTabbedPane();
+        JTabbedPane order_pane = new JTabbedPane();
+
+        JButton button_price = new JButton();
+        left_panel.add(button_price);
+
+        
 
         for (String category : categories) {
-            JPanel panel = new JPanel(new FlowLayout());
+            JPanel panel = new JPanel(new GridLayout(35, 1)); 
+            JPanel order_panel = new JPanel (new GridLayout(1, 1));
+
+            /*
             for (String ctest : categories) {
                 JButton button = new JButton(ctest);
                 panel.add(button);
+            }  */
+            menu_pane.add(category, panel); 
+            for (ArrayList<String> items : menu_items) {
+                JButton button = new JButton(items.get(1));
+                
+                panel.add(button);
+                
+                
+                button.addActionListener(new ActionListener() {
+                    
+                    public void actionPerformed(ActionEvent e) {
+                        //OrderItem order_item = new OrderItem(items.get(1),items.get(2));
+                        //order_items.add(order_item); 
+
+                        String order_item = items.get(1) + " " + items.get(2); //creates item to add to order list
+                        DefaultListModel<String> order_list = new DefaultListModel<>();
+                        order_list.addElement(order_item); //adds order to the list
+                         
+                        JList<String> corder = new JList<>(order_list);  //creates displayed list 
+                        //corder.setSize(150,150); 
+                        left_panel.add(corder); //adds list to left panel
+
+                        //double order_total = 0;
+                        double single_price = Double.parseDouble(items.get(2));
+                        //order_total += single_price;
+
+                        order_prices.add(single_price);
+                        double order_total = 0.0;
+                        for (double prices : order_prices) {
+                            order_total += prices;
+                            String s_order_total = String.valueOf(order_total);
+                            button_price.setText(s_order_total);
+                        }
+
+                        //String s_order_total = String.valueOf(order_total);
+                        
+                        
+                        //JButton button_price = new JButton(String.valueOf(order_total));
+                        //order_panel.add(button_price);
+                    }
+                });
             }
-            menu_pane.add(category, panel);
         }
-        
-        right_panel.add(menu_pane);
+        right_panel.add(menu_pane); 
     }
 }
