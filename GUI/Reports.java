4a6dc1b6c504b8d15ad41e03fa840888a161dfa7
Austin Veselka <austin.veselka@tamu.edu>
2023-03-21 22:37:40 -0500

    Added XZ reports. All reports functional.

--- GUI/Reports.java
+++ GUI/Reports.java
@@ -11,8 +11,11 @@ import javax.swing.*;
 import javax.swing.table.TableModel;
 import java.util.*;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 
 
@@ -342,6 +345,9 @@ public class Reports extends JFrame {
 
         xz_report.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                JTable table;
+                Connection conn;
+                PreparedStatement update_row;
                 
                 JFrame report_frame = new JFrame("XZ Report");
                 report_frame.setSize(600, 800);
@@ -350,77 +356,83 @@ public class Reports extends JFrame {
 
                 // Fill in generating your report and adding to the report frame
                 try {
-                    PreparedStatement update_row;
-                    Connection conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_team_22", "csce315331_team_22_master", "0000");
-                    JTable table = new JTable();
+                    conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315331_team_22", "csce315331_team_22_master", "0000");
+                    table = new JTable();
                     table.getTableHeader().setReorderingAllowed(false);
-        
+
                     Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
                     ResultSet order_summary = stmt.executeQuery("SELECT * FROM orders_summary");
                     DefaultTableModel model = new DefaultTableModel();
+                    model.addColumn("report type");
                     model.addColumn("date");
                     model.addColumn("total_sales");
-        
+
                     double total_sales = 0;
                     order_summary.next();
                     String[] prev_splitted = order_summary.getString(3).split(" ");
                     order_summary.previous();
-        
+
                     while(order_summary.next()) {
                         String[] splitted = order_summary.getString(3).split(" ");
-                        System.out.println(splitted);
-                        for (String item: splitted) {
-                            System.out.println(item);
-                        }
-                        Object[] row = new Object[2];
+                        Object[] row = new Object[3];
                         LocalTime myobj = LocalTime.now();
-                        //handles old z reports
-                        if(splitted[0] != prev_splitted[0] && myobj.getHour() > 12 && myobj.getHour()-12 > 5) {
-                            
-                            update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
-                            update_row.setDouble(1, total_sales);
-                            update_row.setString(2, prev_splitted[0]);
-                            update_row.executeUpdate();  
-        
-                            row[0] = prev_splitted[0];
-                            row[1] = total_sales;
-                            model.addRow(row);
-        
-                            total_sales = 0; 
-                            prev_splitted=splitted;
+                        ZoneId zonedId = ZoneId.of( "America/Montreal" );
+                        LocalDate today = LocalDate.now( zonedId );
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                        String formattedString = today.format(formatter); 
+
+                        //handles old z reports 
+                        //myobj.getHour() > 12 && myobj.getHour()-12 > 5
+                        total_sales += order_summary.getDouble(4);
+                        if(prev_splitted[0].equals(splitted[0]) == false) {
+                            // update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
+                            // update_row.setString(1, prev_splitted[0]);
+                            // update_row.setDouble(2, total_sales);
+                            // update_row.executeUpdate();  
+                            if(splitted[0].equals(formattedString)) {
+                                row[0] = "x report";
+                                row[1] = splitted[0];
+                                row[2] = total_sales;
+                                model.addRow(row);
+                                prev_splitted = splitted;
+                            }
+                            else {
+                                row[0] = "z report";
+                                row[1] = splitted[0];                
+                                row[2] = total_sales;
+                                model.addRow(row);
+
+                                total_sales = 0; 
+                                prev_splitted=splitted;
+                            }
                         }
                         //handles old x reports if they're needed 
-                        else if(splitted[0] != prev_splitted[0] && myobj.getHour()-12 < 5) {
-                            row[0] = prev_splitted[0];
-                            row[1] = total_sales;
-                            model.addRow(row);
-                        }
-                        else {
-                            total_sales += order_summary.getDouble(4);
-                            prev_splitted = splitted;
-                            //handles new z reports
-                            if(myobj.getHour() > 12 && myobj.getHour()-12 > 5) {
-                                update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
-                                update_row.setString(1, prev_splitted[0]);
-                                update_row.setDouble(2, total_sales);
-                                update_row.executeUpdate();  
-        
-                                row[0] = prev_splitted[0];
-                                row[1] = total_sales;
-                                model.addRow(row);
-        
-                                total_sales = 0; 
-                            }
-                            //handles x reports
-                            else{
-                                row[0] = prev_splitted[0];
-                                row[1] = total_sales;
-                                model.addRow(row);
-                            }
-                        }
-                        
-                    }
                     
+                        
+                        // else {
+                            
+                        //     prev_splitted = splitted;
+                        //     //handles new z reports
+                        //     // update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
+                        //     // update_row.setString(1, prev_splitted[0]);
+                        //     // update_row.setDouble(2, total_sales);
+                        //     // update_row.executeUpdate();  
+                        //     row[0] = "x report";
+                        //     row[1] = prev_splitted[0];
+                        //     row[2] = total_sales;
+                        //     model.addRow(row);
+
+                        //     total_sales = 0; 
+                        //     }
+                            //handles x reports
+                            // else{
+                            //     row[0] = "x report";
+                            //     row[1] = prev_splitted[0];
+                            //     row[2] = total_sales;
+                            //     model.addRow(row);  
+                            // }
+                        }
+
                     table.setModel(model);
                     table.setRowHeight(30);
                     JTableHeader header = table.getTableHeader();
@@ -428,14 +440,14 @@ public class Reports extends JFrame {
                     header.setForeground(Color.white);
                     JScrollPane scrollPane = new JScrollPane(table);
                     scrollPane.setColumnHeaderView(header);
-        
+
                     report_frame.add(scrollPane);
                     //Config frame
-                    report_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    // report_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     // report_frame.setSize(800,600);
                     // report_frame.setLocationRelativeTo(null);
-                    // report_frame.setVisible(true);
-                } 
+                    report_frame.setVisible(true);
+                }
                 catch (Exception exc) {
                     exc.printStackTrace();
                 }
--- GUI/XZ.java
+++ GUI/XZ.java
@@ -3,7 +3,11 @@ import java.awt.*;
 import java.sql.*;
 import javax.swing.table.*;
 import javax.swing.table.DefaultTableModel;
+
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class XZ extends JFrame{
     //from what I can tell, x report is sales from last EOD till point of the current day x report is requested
@@ -22,6 +26,7 @@ public class XZ extends JFrame{
             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet order_summary = stmt.executeQuery("SELECT * FROM orders_summary");
             DefaultTableModel model = new DefaultTableModel();
+            model.addColumn("report type");
             model.addColumn("date");
             model.addColumn("total_sales");
 
@@ -32,53 +37,65 @@ public class XZ extends JFrame{
 
             while(order_summary.next()) {
                 String[] splitted = order_summary.getString(3).split(" ");
-                Object[] row = new Object[2];
+                Object[] row = new Object[3];
                 LocalTime myobj = LocalTime.now();
-                //handles old z reports
-                if(splitted[0] != prev_splitted[0] && myobj.getHour() > 12 && myobj.getHour()-12 > 5) {
-                    update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
-                    update_row.setDate(2, java.sql.Date.valueOf(prev_splitted[0]));
-                    update_row.setDouble(1, total_sales);
-                    update_row.executeUpdate();  
+                ZoneId zonedId = ZoneId.of( "America/Montreal" );
+                LocalDate today = LocalDate.now( zonedId );
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                String formattedString = today.format(formatter); 
 
-                    row[0] = prev_splitted[0];
-                    row[1] = total_sales;
-                    model.addRow(row);
-
-                    total_sales = 0; 
-                    prev_splitted=splitted;
-                }
-                //handles old x reports if they're needed 
-                else if(splitted[0] != prev_splitted[0] && myobj.getHour()-12 < 5) {
-                    row[0] = prev_splitted[0];
-                    row[1] = total_sales;
-                    model.addRow(row);
-                }
-                else {
-                    total_sales += order_summary.getDouble(4);
-                    prev_splitted = splitted;
-                    //handles new z reports
-                    if(myobj.getHour() > 12 && myobj.getHour()-12 > 5) {
-                        update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
-                        update_row.setString(1, prev_splitted[0]);
-                        update_row.setDouble(2, total_sales);
-                        update_row.executeUpdate();  
-
-                        row[0] = prev_splitted[0];
-                        row[1] = total_sales;
+                //handles old z reports 
+                //myobj.getHour() > 12 && myobj.getHour()-12 > 5
+                total_sales += order_summary.getDouble(4);
+                if(prev_splitted[0].equals(splitted[0]) == false) {
+                    // update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
+                    // update_row.setString(1, prev_splitted[0]);
+                    // update_row.setDouble(2, total_sales);
+                    // update_row.executeUpdate();  
+                    if(splitted[0].equals(formattedString)) {
+                        row[0] = "x report";
+                        row[1] = splitted[0];
+                        row[2] = total_sales;
+                        model.addRow(row);
+                        prev_splitted = splitted;
+                    }
+                    else {
+                        row[0] = "z report";
+                        row[1] = splitted[0];                
+                        row[2] = total_sales;
                         model.addRow(row);
 
                         total_sales = 0; 
-                    }
-                    //handles x reports
-                    else{
-                        row[0] = prev_splitted[0];
-                        row[1] = total_sales;
-                        model.addRow(row);
+                        prev_splitted=splitted;
                     }
                 }
+                //handles old x reports if they're needed 
+               
                 
-            }
+                // else {
+                    
+                //     prev_splitted = splitted;
+                //     //handles new z reports
+                //     // update_row = conn.prepareStatement("INSERT INTO z_reports VALUES (?, ?)");
+                //     // update_row.setString(1, prev_splitted[0]);
+                //     // update_row.setDouble(2, total_sales);
+                //     // update_row.executeUpdate();  
+                //     row[0] = "x report";
+                //     row[1] = prev_splitted[0];
+                //     row[2] = total_sales;
+                //     model.addRow(row);
+
+                //     total_sales = 0; 
+                //     }
+                    //handles x reports
+                    // else{
+                    //     row[0] = "x report";
+                    //     row[1] = prev_splitted[0];
+                    //     row[2] = total_sales;
+                    //     model.addRow(row);  
+                    // }
+                }
+              
             
             table.setModel(model);
             table.setRowHeight(30);
@@ -101,9 +118,9 @@ public class XZ extends JFrame{
 
     }
 
+
     public static void main(String[] args){
     new XZ();
 }
 
 }
-
